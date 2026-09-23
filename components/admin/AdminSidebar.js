b28438197eb3ce284b2c8b18bc36e4dfde71c198
HassanBuTt78 <herobutt2020@gmail.com>
2026-09-23 21:06:17 +0500

    fix: logout

--- components/admin/AdminSidebar.js
+++ components/admin/AdminSidebar.js
@@ -21,7 +21,7 @@ export default function AdminSidebar() {
     localStorage.removeItem("isAdminLoggedIn");
     localStorage.removeItem("canteen_admin");
     localStorage.removeItem("canteen_token");
-    router.push("/admin/login");
+    router.refresh();
   };
 
   const navItems = [
